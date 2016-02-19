100184dd4bb44633358e344708108d1ba3bf3f8e
Klas Eskilson <klas.eskilson@gmail.com>
2016-02-19 14:53:45 +0100

    Make sure images are properly stacked when image dimensions are even

--- helpers/stack_image.m
+++ helpers/stack_image.m
@@ -2,16 +2,19 @@ function [stacked_image, dimesions] = stack_image(input_image, patch_width)
 
 dimesions = floor(size(input_image) / patch_width);
 
-stacked_image = zeros(patch_width, patch_width, 3, dimesions(1)*dimesions(2));
+% 4D matrix for stacked image
+stacked_image = zeros(patch_width, patch_width, 3, dimesions(1) * dimesions(2));
 counter = 1;
 for y = 1:patch_width:size(input_image, 2)
     for x = 1:patch_width:size(input_image, 1)
-        x_range = x:(x+patch_width-1);
-        y_range = y:(y+patch_width-1);
-        if x+patch_width < size(input_image,1) & y+patch_width < size(input_image,2)
-            stacked_image(:,:,:,counter) = input_image(x_range,y_range,:);
+        x_end = (x + patch_width - 1);
+        y_end = (y + patch_width - 1);
+        x_range = x:x_end;
+        y_range = y:y_end;
+        if x_end <= size(input_image, 1) & y_end <= size(input_image, 2)
+            stacked_image(:, :, :, counter) = input_image(x_range, y_range, :);
+            counter = counter + 1;
         end
-        counter = counter + 1;
     end
 end
 
