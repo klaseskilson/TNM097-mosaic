9fbbd53a99ad740653ad94894b3ac5f1370f4ed0
Klas Eskilson <klas.eskilson@gmail.com>
2016-02-19 11:08:20 +0100

    Add colorspace converting and slicing to mosaic.m

--- mosaic.m
+++ mosaic.m
@@ -2,6 +2,17 @@ function [mosaic] = mosaic(img)
 %MOSAIC(img) Create an mosaic of the RGB image img
 %   Taking the RGB image `img`, an mosaic image is created using the image
 %   database in palette.mat
+    addpath('helpers');
+    load('palette.mat');
+    
+    img = rgb2lab(img);
+    tiles = slice_motif(img, 20);
+    
+    
+    
+    for i = 1:size(tiles, 4)
+        % find best fitting small image
+    end;
 
     mosaic = img;
 end
