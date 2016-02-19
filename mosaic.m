1c0c3b6a240d36742e37f2dc48d41a5828d1cd73
Klas Eskilson <klas.eskilson@gmail.com>
2016-02-19 14:53:18 +0100

    Add debug output to mosaic

--- mosaic.m
+++ mosaic.m
@@ -8,17 +8,21 @@ function [mosaic] = mosaic(img)
     
     tile_width = 20;
     img = rgb2lab(img);
+    disp(['Stacking image...'])
     [stacked_image, dimensions] = stack_image(img, tile_width);
 %     for i = 1:numel(db)
 %         db{i} = imresize(db{i}, [tile_width tile_width]);
 %     end
+    disp(['Getting mean values for palette...'])
     mean_values = get_mean(palette, tile_width);
     
+    disp(['Matching patches...'])
     for i = 1:size(stacked_image, 4)
         % find best fitting small image
         index = find_match(mean_lab(stacked_image(:,:,:,i)), mean_values);
         mosaic_stack(:,:,:,i) = imresize(palette{index}, [tile_width tile_width]);
     end;
 
+    disp(['Unstacking image...'])
     mosaic = lab2rgb(unstack_image(mosaic_stack, dimensions));
 end
